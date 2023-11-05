da8102c96fe70477591d96600f79f710ec5a3cde
Paulo Keller <paulo.vinicius.keller@gmail.com>
2023-11-05 17:04:05 -0300

    add the cinemachine follow camera, create the player jump, add the enemy and the die behavior

--- Assets/Scripts/Player/PlayerMoviment.cs
+++ Assets/Scripts/Player/PlayerMoviment.cs
@@ -1,33 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMoviment : MonoBehaviour
 {
-    Rigidbody2D playerRigidbody2D;
-    Vector2 moveInput;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    Rigidbody2D playerRigidbody2D;
+    CapsuleCollider2D playerCapsuleCollider2D;
+    Vector2 moveInput;
+    bool isAlive = true;
 
     void Start()
     {
         playerRigidbody2D = GetComponent<Rigidbody2D>();
+        playerCapsuleCollider2D = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
+        if (!isAlive) { return; }
+
         Run();
+        Die();
+    }
+
+    void OnJump(InputValue value) 
+    {
+        HandleJump(value);
+    }
+
+    void HandleJump(InputValue value) 
+    {
+        if (!playerCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+
+        if (value.isPressed) 
+        {
+            playerRigidbody2D.velocity += new Vector2(0f, jumpSpeed);
+        }
     }
 
     void OnMove(InputValue value) 
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
     }
 
     void Run() 
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, playerRigidbody2D.velocity.y);
         playerRigidbody2D.velocity = playerVelocity;
+    }
+    void Die() 
+    {
+        if (playerCapsuleCollider2D.IsTouchingLayers(LayerMask.GetMask("Enemies"))) 
+        {
+            isAlive = false;
+        }
     }
 }
